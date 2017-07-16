2ed4e8a81977d54cb9da97402be41cb81cef6484
Tan <tmx0456@gamil.com>
2017-07-16 18:09:30 +0800

    first commit

---
+++ TanLibrary/src/main/java/app/newbee/lib/rx/subscribe/ViewStubEventOnSubscribe.java
@@ -0,0 +1,38 @@
+package app.newbee.lib.rx.subscribe;
+
+import android.view.View;
+import android.view.ViewStub;
+import app.newbee.lib.rx.rxviewstub.ViewStubEvent;
+import com.jakewharton.rxbinding.internal.MainThreadSubscription;
+import com.jakewharton.rxbinding.internal.Preconditions;
+import rx.Observable;
+import rx.Subscriber;
+
+public class ViewStubEventOnSubscribe implements Observable.OnSubscribe<ViewStubEvent> {
+
+  private ViewStub viewStub;
+
+  public ViewStubEventOnSubscribe(ViewStub viewStub) {
+    this.viewStub = viewStub;
+  }
+
+  @Override public void call(final Subscriber<? super ViewStubEvent> subscriber) {
+
+    Preconditions.checkUiThread();
+
+    ViewStub.OnInflateListener inflateListener = new ViewStub.OnInflateListener() {
+      @Override public void onInflate(ViewStub stub, View inflated) {
+        if (!subscriber.isUnsubscribed()) {
+          subscriber.onNext(ViewStubEvent.create(stub, inflated));
+        }
+      }
+    };
+    viewStub.setOnInflateListener(inflateListener);
+
+    subscriber.add(new MainThreadSubscription() {
+      @Override protected void onUnsubscribe() {
+        viewStub.setOnInflateListener(null);
+      }
+    });
+  }
+}
